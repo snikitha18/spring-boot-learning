8c1f4099a8624dfb160dc58cccbe4ad90c1a70cc
snikitha18 <niki20ambalpady@gmail.com>
2025-07-22 06:14:26 +0530

    JWT Token validation and securing API

--- src/main/java/com/springboot/miniproject/book/utilities/JWTUtility.java
+++ src/main/java/com/springboot/miniproject/book/utilities/JWTUtility.java
@@ -5,8 +5,11 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -45,4 +48,29 @@ public class JWTUtility {
             .signWith(key)  // uses HS256 by default
             .compact();
     }
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+	public String extractUsername(String token) {
+		 return extractAllClaims(token).getSubject();
+	}
+
+	public boolean validateToken(String token, UserDetails user) {
+		 try {
+		        Claims claims = extractAllClaims(token);
+		        String username = claims.getSubject();
+		        Date expiration = claims.getExpiration();
+		        return username.equals(user.getUsername()) && expiration.after(new Date());
+		    } 
+		 catch (JwtException | IllegalArgumentException e) {
+		        return false;  // invalid, tampered, or expired token
+		    }
+	
+	}
 }
